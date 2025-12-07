00a03b5f202acd8bcdaaa1e219a5aecd5009de87
byoncocare <getbyonco@gmail.com>
2025-12-07 23:06:14 +0000

    fix: update CORS import to use fastapi.middleware.cors as requested

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, APIRouter, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-from starlette.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 import logging
